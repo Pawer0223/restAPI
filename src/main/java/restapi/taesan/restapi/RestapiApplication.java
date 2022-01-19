03f577b385f21d0751d4252482c08a66426c4401
taesan <pawer02023@gmail.com>
2022-01-20 02:10:41 +0900

    configure .. WebSecurity 에서 build할 때, Test깨져서 index.html안만들어져서 404 때문에 login 으로 가짐, 강제로 docs/index.html 만들면 정상적으로 접근 가능함

--- src/main/java/restapi/taesan/restapi/RestapiApplication.java
+++ src/main/java/restapi/taesan/restapi/RestapiApplication.java
@@ -1,21 +1,12 @@
 package restapi.taesan.restapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RestapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestapiApplication.class, args);
     }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 }
